9f788179e92c6fe173237917de271f158b89d935
Laura <lkzailac@gmail.com>
2021-05-23 18:01:36 -0400

    asset seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .assets import seed_assets, undo_assets
+from .transactions import seed_transactions, undo_transactions
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +11,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_assets()
+    seed_transactions()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_assets()
+    undo_transactions()
     # Add other undo functions here
