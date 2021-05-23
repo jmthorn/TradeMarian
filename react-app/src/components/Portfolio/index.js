c5f75a5d254c0c0e445ba513ca83e89a90cba0d5
Jessica T <jmthorn16@gmail.com>
2021-05-22 21:13:05 -0500

    splash and portfolio routes added

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { useSelector } from 'react-redux'
-import './splash.css';
+import './portfolio.css';
 
-const Splash = () => {
+const Portfolio = () => {
 
   const user = useSelector(state => state.session.user)
 
+
   return (
       <>
-        <h1>Splash</h1>
+        <h1>Portfolio</h1>
       </>
   )
 };
 
-export default Splash;
+export default Portfolio;
