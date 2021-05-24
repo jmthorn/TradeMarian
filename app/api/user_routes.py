c3b017da7d45d14f62252f5686cf34c8ac5a678f
Jessica T <jmthorn16@gmail.com>
2021-05-24 18:20:21 -0500

    Holdings and FilteredHoldings backend functional

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -4,6 +4,8 @@ from app.models import User, transaction
 from app.models import Transaction
 from app.models import Asset
 import json
+import requests
+import os
 
 user_routes = Blueprint('users', __name__)
 
@@ -19,13 +21,53 @@ user_routes = Blueprint('users', __name__)
 @login_required
 def user():
 
+    # Current user
     user = User.query.get(current_user.id).to_dict()
 
+    # Holdings: We need total stocks owned by current_user in format: { AAPL:30, GOOG:25, SRP:14}
     userid = current_user.id
     transactions = Transaction.query.filter(Transaction.user_id == userid).all()
-    print(transactions[1].asset.to_dict())
+    # print(transactions[1].asset.ticker_symbol)
+    # print(transactions[1].to_dict()) #{'id': 2, 'asset_id': 2, 'share_quantity': 7, 'price_per_share': 245, 'buy_sell': True, 'created_at': datetime.datetime(2021, 5, 24, 16, 31, 50, 743094)}
+    
+    
+    holdings = {}
+    for transaction in transactions:
+        if transaction.asset.ticker_symbol in holdings:
+            holdings[transaction.asset.ticker_symbol] += transaction.share_quantity
+        else:
+            holdings[transaction.asset.ticker_symbol] = transaction.share_quantity
+
+    print(holdings)  #{'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'GE': 0, 'AMZN': 3, 'DIS': 11}
+
+    filteredHoldings = {key:holdings[key] for key in holdings if holdings[key] > 0}
+
+    print(filteredHoldings) # {'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'AMZN': 3, 'DIS': 11}
+
+    # History: Loop over holdings to find history per ticker_symbol 
+    # {AAPL: [{date: 03-06-2021, value:"close * holding.AAPL"}, {date: 03-06-2021, value:"close * holding.AAPL"},......]}, 
+    #  GOOG: [{date: 03-06-2021, value:"close * holding.GOOG"}, {date: , value: },{date: , value: }.......], }
+
+    daterange="1y"
+    symbol="AAPL"
+    api_token=os.environ.get("API_TOKEN")
+
+    # response = requests.get(f"https://sandbox.iexapis.com/stable/stock/{symbol}/chart/{daterange}?token={api_token}&chartCloseOnly=true")
+
+    # print(response.json())
+
+    # Porfolio: (TOTALED history) : {date:value, date:value, ........}
+    # portfolio = {}
+    # 
+    # 
+    #      
+    #       
+    #          
+    #       
+    #          
+
     # print({i:transactions[i].to_dict() for i in range(0, len(transactions))})
     # add up the transactions for each ticker
     # so this gives us our totals and lets us know which companies to make API
-    {user, history, [aapl, ...]}
+    # {user, history, portfolio}
     return {i:transactions[i].to_dict() for i in range(0, len(transactions))}
