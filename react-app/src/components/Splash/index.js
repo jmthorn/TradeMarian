7fc487764225551ddbfbe1996cae2b8c2640de5f
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 20:24:37 -0700

    splash, footer, and nav responsive

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -13,24 +13,24 @@ const Splash = () => {
 
   return (
       <>
-          <div id="splash-blue">
-              <div id="splash-description">
-                  <h1>Investing for</h1>
-                  <h1>Everyone</h1>
-                  <div id="h1-spacer"></div>
-                  <p>Commission-free investing, plus the tools</p>
-                  <p>you need to put your money in motion. Sign</p>
-                  <p>up and get your first stock for free. Certain</p>
-                  <p>limitations apply.</p>
-                  <button onClick={handleSubmit}>Sign Up</button>
-              </div>
-              <div id="image-container">
-                <img height="600em" src="images/Splash_Image.png" alt="splash_img"></img>
-                  <video id="video" loop="true" autoplay="autoplay" muted>
-                    <source src="/images/Iphone_Animation.mp4" type="video/mp4"></source>
-                  </video>
-              </div>
-          </div>
+        <div id="splash-blue">
+            <div id="splash-description">
+                <h1>Investing for</h1>
+                <h1>Everyone</h1>
+                <div id="h1-spacer"></div>
+                <p>Commission-free investing, plus the tools</p>
+                <p>you need to put your money in motion. Sign</p>
+                <p>up and get your first stock for free. Certain</p>
+                <p>limitations apply.</p>
+                <button onClick={handleSubmit}>Sign Up</button>
+            </div>
+            <div id="image-container">
+              <img height="600em" src="images/Splash_Image.png" alt="splash_img"></img>
+                <video id="video" loop="true" autoplay="autoplay" muted>
+                  <source src="/images/Iphone_Animation.mp4" type="video/mp4"></source>
+                </video>
+            </div>
+        </div>
         <div id="splash-white">
           <img height="600em" src="images/user.png" alt="user_img"></img>
           <div id="turn">
