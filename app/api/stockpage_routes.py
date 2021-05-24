96b8489f35b116444b5e4400cff161a34031631b
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-24 10:51:13 -0700

    api and route started

--- app/api/stockpage_routes.py
+++ app/api/stockpage_routes.py
@@ -1,20 +1,17 @@
 from flask import Blueprint
 import requests
-from .env import test_token
+import os
 
 stock_routes = Blueprint("stock", __name__)
 
-reqs = requests.get(
-    f"https://sandbox.iexapis.com/stable/stock/aapl/company?token={test_token}")
-
-print(reqs.content)
+stock_token = os.environ.get('STOCK_API')
 
 
-# @stock_routes.route('/stocks/<ticker_symbol>')
-# def stock_data(ticker_symbol):
-#     reqs = requests.get(
-#         f"https://sandbox.iexapis.com/stable/stock/{ticker_symbol}/company?token={test_token}")
+@stock_routes.route('/stocks/<ticker_symbol>')
+def stock_data(ticker_symbol):
+    reqs = requests.get(
+        f"https://cloud.iexapis.com/stable/stock/aapl/chart/1m/?token={stock_token}")
 
-#     print(reqs.content)
+    print(reqs.content)
 
-#     # return
+    # return
