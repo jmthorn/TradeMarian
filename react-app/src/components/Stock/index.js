58cfc5335feda3dc2302a40f248a255d0710b3d7
Tran <tranle97@gmail.com>
2021-05-24 19:05:19 -0500

    Refactor function

--- react-app/src/components/Stock/index.js
+++ react-app/src/components/Stock/index.js
@@ -5,8 +5,7 @@ import { stockPrices } from "../../store/assets";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import './stock.css';
 
-
-const StockPage = () => {
+const Stock = () => {
     const { ticker_symbol } = useParams();
     const prices = useSelector(state => state.assets.historicalPrices);
     console.log('-----------', prices);
@@ -22,26 +21,27 @@ const StockPage = () => {
             //         closePrice.push(obj[key])
             //     }
             // }
+        }
     }
 
-    console.log('test----',closePrice)
-    useEffect(() => {
-        dispatch(stockPrices(ticker_symbol.toUpperCase()))
-    }, [dispatch, ticker_symbol])
+        console.log('test----', closePrice)
+        useEffect(() => {
+            dispatch(stockPrices(ticker_symbol.toUpperCase()))
+        }, [dispatch, ticker_symbol])
 
-    return (
-        <div id='stock-container'>
-            <LineChart width={730} height={250} data={closePrice}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis dataKey="price" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="equity" stroke="#8884d8" />
-            </LineChart>
-        </div>
-    )
-}
+        return (
+            <div id='stock-container'>
+                <LineChart width={730} height={250} data={closePrice}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" />
+                    <YAxis dataKey="price" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="equity" stroke="#8884d8" />
+                </LineChart>
+            </div>
+        )
+    }
 
-export default StockPage;
+    export default Stock;
