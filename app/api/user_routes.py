98cd9ccd24b37d14ecf5601eb7d12f1270697042
Jessica T <jmthorn16@gmail.com>
2021-05-25 11:40:04 -0500

    figured out portfolio calculations

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -38,11 +38,11 @@ def user():
         else:
             holdings[transaction.asset.ticker_symbol] = transaction.share_quantity
 
-    print(holdings)  #{'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'GE': 0, 'AMZN': 3, 'DIS': 11}
+    # print(holdings)  #{'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'GE': 0, 'AMZN': 3, 'DIS': 11}
 
     shares = {key:holdings[key] for key in holdings if holdings[key] > 0}
 
-    print(shares) # {'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'AMZN': 3, 'DIS': 11}
+    # print(shares) # {'AAPL': 5, 'MSFT': 7, 'TSLA': 10, 'SPOT': 4, 'AMZN': 3, 'DIS': 11}
 
     # History: Loop over holdings to find history per ticker_symbol 
     # {AAPL: [{date: 03-06-2021, value:"close * holding.AAPL"}, {date: 03-06-2021, value:"close * holding.AAPL"},......]}, 
@@ -59,18 +59,27 @@ def user():
         data = response.json()
         history[symbol] = [{"date":dictionary["date"],"value":dictionary["close"]} for dictionary in data]
 
-    print(history)
+    print("HISTORY.......", history)
 
 
     # Porfolio: (TOTALED history) : {date:value, date:value, ........}
     # portfolio = {}
-    # 
-    # 
-    #      
-    #       
-    #          
-    #       
-    #          
+    # # 
+    obj={}
+    for i in range(len(list(history.values())[0])):
+        for key, stock in history.items():
+            print("SHARES",shares[key])
+            key_value=stock[i] 
+            date=key_value['date']
+            value=key_value['value'] * shares[key]
+            if date in obj:
+                obj[str(date)] += value
+            else:
+                obj[str(date)] = value
+
+    print("OBJ..........",obj)
+
+    # OBJ.......... {'2021-04-26': 23848.579999999998, '2021-04-27': 23335.850000000002, '2021-04-28': 23114.54, '2021-04-29': 23323.47, '2021-04-30': 23661.219999999998, '2021-05-03': 23368.44, '2021-05-04': 22340.160000000003, '2021-05-05': 22201.1, '2021-05-06': 22154.629999999997, '2021-05-07': 22521.329999999998, '2021-05-10': 21612.950000000004, '2021-05-11': 21525.390000000003, '2021-05-12': 21137.4, '2021-05-13': 21149.949999999997, '2021-05-14': 21515.190000000002, '2021-05-17': 21246.09, '2021-05-18': 21215.46, '2021-05-19': 20702.030000000002, '2021-05-20': 21434.84, '2021-05-21': 20904.72, '2021-05-24': 21621.379999999997}
 
     # print({i:transactions[i].to_dict() for i in range(0, len(transactions))})
     # add up the transactions for each ticker
