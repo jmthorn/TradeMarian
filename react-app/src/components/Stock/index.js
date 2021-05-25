1acb731902e4f72c7c01296be356efffecaecd5c
Tran <tranle97@gmail.com>
2021-05-25 16:35:32 -0500

    add three date graphs

--- react-app/src/components/Stock/index.js
+++ react-app/src/components/Stock/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { stockPrices } from "../../store/assets";
@@ -10,6 +10,7 @@ const Stock = () => {
     const { ticker_symbol } = useParams();
     const prices = useSelector(state => state.assets.historicalPrices);
     const dispatch = useDispatch();
+    const [dateRange, setDateRange] = useState(prices)
 
     useEffect(() => {
         dispatch(stockPrices(ticker_symbol.toUpperCase()))
@@ -22,9 +23,19 @@ const Stock = () => {
         lineColor = "#97ef0c";
     }
 
+    const dateFunc = (date) => {
+    if(date == '1Y') {
+        setDateRange(prices)
+    } else if (date == '6m') {
+        setDateRange(prices.slice(0, prices.length / 2))
+    } else if (date == '1m') {
+        setDateRange(prices.slice(0, prices.length / 12))
+    }
+  }
+
     return (
         <div id='stock-container'>
-            <LineChart width={730} height={250} data={prices}
+            <LineChart width={730} height={250} data={dateRange}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid horizontal={false} vertical={false} />
                 <XAxis dataKey="close" hide={true} />
@@ -32,6 +43,15 @@ const Stock = () => {
                 <Tooltip />
                 <Line type="monotone" dataKey="close" stroke={lineColor} strokeWidth={1.5} dot={false} />
             </LineChart>
+            <div>
+                <button type='button' value='1m' onClick={(e) => dateFunc(e.target.value)}>1m</button>
+            </div>
+            <div>
+                <button type='button' value='6m' onClick={(e) => dateFunc(e.target.value)}>6m</button>
+            </div>
+            <div>
+                <button type='button' value='1Y' onClick={(e) => dateFunc(e.target.value)}>1Y</button>
+            </div>
             <BuySell ticker_symbol={ticker_symbol.toUpperCase()} />
         </div>
     )
