75c25dee7cd062c1423a2cf06192081823023aad
Jessica T <jmthorn16@gmail.com>
2021-05-25 12:33:30 -0500

    large portfolio chart rendered

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -59,7 +59,7 @@ def user():
         data = response.json()
         history[symbol] = [{"date":dictionary["date"],"value":dictionary["close"]} for dictionary in data]
 
-    print("HISTORY.......", history)
+    # print("HISTORY. ......", history)
 
 
     # Porfolio: (TOTALED history) : {date:value, date:value, ........}
@@ -76,15 +76,19 @@ def user():
             else:
                 obj[str(date)] = value
 
-    print("OBJ..........",obj)
+    # print("OBJ..........",obj)
 
     # OBJ.......... {'2021-04-26': 23848.579999999998, '2021-04-27': 23335.850000000002, '2021-04-28': 23114.54, '2021-04-29': 23323.47, '2021-04-30': 23661.219999999998, '2021-05-03': 23368.44, '2021-05-04': 22340.160000000003, '2021-05-05': 22201.1, '2021-05-06': 22154.629999999997, '2021-05-07': 22521.329999999998, '2021-05-10': 21612.950000000004, '2021-05-11': 21525.390000000003, '2021-05-12': 21137.4, '2021-05-13': 21149.949999999997, '2021-05-14': 21515.190000000002, '2021-05-17': 21246.09, '2021-05-18': 21215.46, '2021-05-19': 20702.030000000002, '2021-05-20': 21434.84, '2021-05-21': 20904.72, '2021-05-24': 21621.379999999997}
+    # [{'date':'2021-04-26', "value":23848.579999999998}]
 
-    
+    portfolio_data = [{"date":key, "value": value} for (key,value) in obj.items()]
+    # portfolio_data = {}
+    # for key, value in obj.items():
 
 
+    # print("PORTFOLIO DATA", portfolio_data)
     # print({i:transactions[i].to_dict() for i in range(0, len(transactions))})
     # add up the transactions for each ticker
     # so this gives us our totals and lets us know which companies to make API
     # {user, history, portfolio}
-    return {"shares":shares, "history":history}
+    return {"shares":shares, "history":history, "portfolio_data":portfolio_data}
--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import './portfolio.css';
 import { userPortfolio } from "../../store/portfolio"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 import Sidebar from './Sidebar'
 
 const Portfolio = () => {
 
   const user = useSelector(state => state.session.user)
+  const portfolio_data = useSelector(state => state.portfolio.portfolio?.portfolio_data)
   const dispatch = useDispatch()
 
-  const onLoad = async () => {
-    const portfolio = await dispatch(userPortfolio())
-    // console.log(portfolio)
-  }
 
-  onLoad()
+  useEffect(async() => { 
+    const portfolio = await dispatch(userPortfolio())
+  }, [])
+
+
 
   return (
       <>
         <h1>$Portfolio</h1>
+          <div className="portfolio-container">
+              <div>
+                  <LineChart width={730} height={250} data={portfolio_data}
+                      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                      <Line type="monotone"  dataKey="value" stroke="#8884d8" strokeWidth={2} dot={false}/>
+                      {/* <CartesianGrid stroke="#ccc" strokeDasharray="3 3" hide={true}/> */}
+                      <XAxis dataKey="date"  hide={true}/>
+                      <YAxis dataKey="value" domain={['auto', 'auto']} hide={true}/>
+                      <Tooltip />
+                      {/* <Legend /> */}
+                  </LineChart>
+              </div>
+          </div>
         <Sidebar/>
       </>
   )
 };
 
 export default Portfolio;
+
+
+
+{/* <div id='stock-container'>
+            <LineChart width={730} height={250} data={prices}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid horizontal={false} vertical={false} />
+                <XAxis dataKey="close" hide={true} />
+                <YAxis domain={['auto', 'auto']} hide={true} />
+                <Tooltip />
+                <Line type="monotone" dataKey="close" stroke={lineColor} strokeWidth={1.5} dot={false}/>
+            </LineChart>
+        </div> */}
