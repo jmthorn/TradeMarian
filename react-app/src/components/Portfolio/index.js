894004b86a929ce2c7ba2d02b61d13fbd4657d53
Jessica T <jmthorn16@gmail.com>
2021-05-25 13:23:39 -0500

    fixed portfolio line color

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import './portfolio.css';
 import { userPortfolio } from "../../store/portfolio"
@@ -7,8 +7,9 @@ import Sidebar from './Sidebar'
 
 const Portfolio = () => {
 
+  const [lineColor, setLineColor] =  useState("")
   const user = useSelector(state => state.session.user)
-  const portfolio_data = useSelector(state => state.portfolio.portfolio?.portfolio_data)
+  const portfolio_data = useSelector(state => state.portfolio?.portfolio?.portfolio_data)
   const dispatch = useDispatch()
 
 
@@ -18,6 +19,23 @@ const Portfolio = () => {
 
 
 
+  useEffect(async()=> { 
+    const portfolio_array = await portfolio_data;
+    console.log(portfolio_array)
+    if(portfolio_array){
+      if (portfolio_array[0].value > portfolio_array[(portfolio_array.length)-1].value) {
+        setLineColor("#dc436f"); //pink
+      } else {
+        setLineColor("#97ef0c"); //green
+      }
+    }
+    
+  }, [portfolio_data, lineColor])
+  console.log(lineColor)
+  
+   if(!portfolio_data) return null
+
+
   return (
       <>
         <h1>$Portfolio</h1>
@@ -25,7 +43,7 @@ const Portfolio = () => {
               <div>
                   <LineChart width={730} height={250} data={portfolio_data}
                       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                      <Line type="monotone"  dataKey="value" stroke="#8884d8" strokeWidth={2} dot={false}/>
+                      <Line type="monotone"  dataKey="value" stroke={lineColor} strokeWidth={2} dot={false}/>
                       {/* <CartesianGrid stroke="#ccc" strokeDasharray="3 3" hide={true}/> */}
                       <XAxis dataKey="date"  hide={true}/>
                       <YAxis dataKey="value" domain={['auto', 'auto']} hide={true}/>
