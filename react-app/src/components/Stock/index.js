ac0abc8c81f0c3c258615a6331c2304c1dfafc58
Tran <tranle97@gmail.com>
2021-05-25 22:21:45 -0500

    Refactor assets route

--- react-app/src/components/Stock/index.js
+++ react-app/src/components/Stock/index.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { stockPrices } from "../../store/assets";
+import { stockInformation } from "../../store/assets";
 import BuySell from './BuySell';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import './stock.css';
 
 const Stock = () => {
     const { ticker_symbol } = useParams();
-    const prices = useSelector(state => state.assets.historicalPrices);
+    const stock = useSelector(state => state.assets.stockData);
+    const stockData = stock.stock_data;
+    console.log(stockData, '=================stockdata')
+    const stockOverview = stock.stock_info;
     const dispatch = useDispatch();
-    const [dateRange, setDateRange] = useState(prices)
+    const [dateRange, setDateRange] = useState(stockData);
 
     useEffect(() => {
-        dispatch(stockPrices(ticker_symbol.toUpperCase()))
-    }, [dispatch, ticker_symbol])
+        dispatch(stockInformation(ticker_symbol.toUpperCase()));
+    }, [dispatch, ticker_symbol]);
+
+    if (!stockData) return null;
 
     let lineColor = "";
-    if (prices[0] > prices[prices.length - 1]) {
+
+    if (stockData[0] > stockData[stockData.length - 1]) {
         lineColor = "#dc436f"; // pink
     } else {
         lineColor = "#97ef0c"; // green
     }
 
+
     const dateFunc = (date) => {
     if(date == '1Y') {
-        setDateRange(prices)
+        setDateRange(stockData)
     } else if (date == '6M') {
-        setDateRange(prices.slice(0, prices.length / 2))
+        setDateRange(stockData.slice(0, stockData.length / 2))
     } else if (date == '1M') {
-        setDateRange(prices.slice(0, prices.length / 12))
+        setDateRange(stockData.slice(0, stockData.length / 12))
+        }
     }
-  }
 
     return (
         <div id='stock-container'>
