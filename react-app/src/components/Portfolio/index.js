e5ba90f46122965d2d3bf6195168aa3de4d881d8
Laura <lkzailac@gmail.com>
2021-05-26 11:13:49 -0400

    added change equity, change percent

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -12,12 +12,17 @@ const Portfolio = () => {
   const portfolio_data = useSelector(state => state.portfolio?.portfolio?.portfolio_data)
   const dispatch = useDispatch()
   const [dateRange, setDateRange] = useState(portfolio_data)
-  const [currentPrice, setCurrentPrice] = useState(portfolio_data ? portfolio_data[(portfolio_data?.length)-1].value : 21541.08)
+  // const [currentPrice, setCurrentPrice] = useState(portfolio_data ? portfolio_data[(portfolio_data?.length)-1].value : 21541.08)
+  const [currentPrice, setCurrentPrice] = useState('')
+  const [currentChange, setCurrentChange] = useState('')
+  const [currentPercentChg, setCurrentPercentChg] = useState('')
+  const [sign, setSign] = useState('')
+  const [timeInterval, setTimeInterval] = useState('')
+  const [past, setPast] = useState('')
 
   useEffect(async() => {
     const portfolio = await dispatch(userPortfolio())
   }, [])
-
 
 
   useEffect(async()=> {
@@ -34,32 +39,67 @@ const Portfolio = () => {
   }, [portfolio_data, lineColor])
 
 
-
   if(!portfolio_data) return null
 
   const dateFunc = (date) => {
     if(date == '1Y') {
+      setTimeInterval('1Y')
       setDateRange(portfolio_data)
     } else if (date == '6m') {
+      setTimeInterval('6m')
       setDateRange(portfolio_data.slice(0, portfolio_data.length/2))
     } else if (date == '1m') {
+      setTimeInterval('1m')
       setDateRange(portfolio_data.slice(0, portfolio_data.length/12))
     } else if (date == '3m') {
+      setTimeInterval('3m')
       setDateRange(portfolio_data.slice(0, portfolio_data.length/4))
     }
   }
 
   const handleClick = (value) => {
     setCurrentPrice(Math.round(value * 100) / 100)
+    // condition on state of dateRange
+    setCurrentChange(Math.round((value - portfolio_data[0].value)*100) /100)
+    // calc percent change
+    if (portfolio_data[(portfolio_data?.length) -1].value > portfolio_data[0].value) {
+      setCurrentPercentChg( Math.round((((portfolio_data[0].value - value) / value)*100) *100) /100)
+    } else {
+      setCurrentPercentChg( Math.round((((value - portfolio_data[0].value) / portfolio_data[0].value)*100) *100) /100 )
+    }
+    //set sign
+    if (portfolio_data[(portfolio_data?.length) -1].value > portfolio_data[0].value) {
+      setSign('+')
+    } else {
+      setSign('-')
+    }
+    //set "Past"
+    if (timeInterval == '1m') {
+      setPast('Past Month')
+    } else if (timeInterval == '3m') {
+      setPast('Past 3 Months')
+    } else if (timeInterval == '6m') {
+      setPast('Past 6 Months')
+    } else if (timeInterval == '1Y') {
+      setPast('Past Year')
+    }
   }
+
 
   return (
       <>
         <div id="page-container">
           <div id="portfolio-container">
             <div id="ticker">
-              <h1 id="">${currentPrice}</h1>
+              <h1 id="">${currentPrice ? currentPrice : portfolio_data[(portfolio_data?.length)-1].value}</h1>
             </div>
+            <div id='ticker-change'>
+              <p>{`${sign}$${ currentChange ? currentChange : Math.round((portfolio_data[(portfolio_data?.length) -1].value - portfolio_data[0].value) * 100) /100 }
+                (${sign}${currentPercentChg ? currentPercentChg : Math.round(((((portfolio_data[0].value - portfolio_data[(portfolio_data?.length) -1].value)/ portfolio_data[(portfolio_data?.length) -1].value) * 100) * 100) /100)  }%)`}
+                { past ? past : 'Past Year'}
+              </p>
+            </div>
+
               <div id="portfolio-graph-container">
                   <div>
                       <LineChart width={730} height={250} data={dateRange ? dateRange : portfolio_data}
