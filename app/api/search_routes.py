0075da51f2e694448e5e7ff1a950e87296193c70
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-26 19:56:03 -0700

    search routes started

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+from app.models import Asset
 
 search_routes = Blueprint("search", __name__)
 
 
-@search_routes.route('/')
-def get_stocks():
-    pass
+@search_routes.route('/<ticker_symbol>')
+def get_stocks(ticker_symbol):
+    stock_name = Asset.query.filter()
