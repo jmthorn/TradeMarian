3695a8257e099926eba09186aa9d6041536988a7
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-27 10:08:13 -0700

    .

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -6,7 +6,7 @@ search_routes = Blueprint("search", __name__)
 
 @search_routes.route('/<search_string>')
 def get_stocks(search_string):
-    stock_name = Asset.query.filter(Asset.ticker_symbol == search_string).one()
+    stock_name = Asset.query.all()
     print(stock_name)
 
     return {stock_name}
