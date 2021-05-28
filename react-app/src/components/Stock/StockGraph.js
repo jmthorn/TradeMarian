aa8239d7f7aad3f9893fd11d616c43c04005630d
Laura <lkzailac@gmail.com>
2021-05-28 16:42:04 -0400

    added stock pricing and button colors

--- react-app/src/components/Stock/StockGraph.js
+++ react-app/src/components/Stock/StockGraph.js
@@ -1,71 +1,133 @@
-import React, { useEffect, useState } from "react";
-// import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+// import React, { useEffect, useState } from "react";
+// // import { useParams } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
 // import { stockInformation } from "../../store/assets";
-import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+// import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
-const StockGraph = () => {
-  const stock = useSelector(state => state.assets.stockData);
-  const stockData = stock.stock_data;
-  const [dateRange, setDateRange] = useState(stockData);
-  const [lineColor, setLineColor] = useState("")
+// const StockGraph = () => {
+//   const stock = useSelector(state => state.assets.stockData);
+//   const stockData = stock.stock_data;
+//   const [dateRange, setDateRange] = useState(stockData);
+//   const [lineColor, setLineColor] = useState("")
+//   const [currentPrice, setCurrentPrice] = useState('')
+//   const [currentChange, setCurrentChange] = useState('')
+//   const [currentPercentChg, setCurrentPercentChg] = useState('')
+//   const [sign, setSign] = useState('')
+//   const [timeInterval, setTimeInterval] = useState('')
+//   const [past, setPast] = useState('')
 
-  let stockDatePrice = []
-  for (let i in stockData) {
-    stockDatePrice.push({ "date": stockData[i]["date"], "price": stockData[i]["price"] })
-  }
+//   const dispatch = useDispatch()
 
-  useEffect( () => {
-    let stockDatePriceArr = dateRange ? dateRange : stockData;
-    if (stockDatePriceArr) {
-      if (stockDatePriceArr[0]["price"] > stockDatePriceArr[(stockDatePriceArr.length) - 1]["price"]) {
-        setLineColor("#dc436f"); //pink
-      } else {
-        setLineColor("#97ef0c"); //green
-      }
-    }
-
-  }, [stockData, lineColor, dateRange])
+//   useEffect(async() => {
+//     await dispatch(stockInformation())
+//   }, [])
 
 
-  const dateFunc = (date) => {
-    if (date === '1Y') {
-      setDateRange(stockData)
-    } else if (date == '6M') {
-      setDateRange(stockData.slice((stockData.length) / 2))
-    } else if (date == '3M') {
-      setDateRange(stockData.slice(((stockData.length) / 4) * 3))
-    } else if (date == '1M') {
-      setDateRange(stockData.slice(((stockData.length) / 12) * 11))
-    }
-  }
+//   let stockDatePrice = []
+//   for (let i in stockData) {
+//     stockDatePrice.push({ "date": stockData[i]["date"], "price": stockData[i]["price"] })
+//   }
 
-  return (
-    <div id='stock-graph-container'>
-      <LineChart width={730} height={250} data={dateRange ? dateRange : stockData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <XAxis dataKey="date" hide={true} />
-        <YAxis dataKey="price" domain={['auto', 'auto']} hide={true} />
-        <Tooltip offset={55} />
-        <Line type="monotone" dataKey="price" stroke={lineColor} strokeWidth={1.5} dot={false} />
-      </LineChart>
-      <div id='stock-graph-btns'>
-        <div>
-          <button type='button' value='1M' onClick={(e) => dateFunc(e.target.value)}>1M</button>
-        </div>
-        <div>
-          <button type='button' value='3M' onClick={(e) => dateFunc(e.target.value)}>3M</button>
-        </div>
-        <div>
-          <button type='button' value='6M' onClick={(e) => dateFunc(e.target.value)}>6M</button>
-        </div>
-        <div>
-          <button type='button' value='1Y' onClick={(e) => dateFunc(e.target.value)}>1Y</button>
-        </div>
-      </div>
-    </div>
-  )
+//   useEffect( () => {
+//     let stockDatePriceArr = dateRange ? dateRange : stockData;
+//     if (stockDatePriceArr) {
+//       if (stockDatePriceArr[0]["price"] > stockDatePriceArr[(stockDatePriceArr.length) - 1]["price"]) {
+//         setLineColor("#dc436f"); //pink
+//       } else {
+//         setLineColor("#97ef0c"); //green
+//       }
+//     }
+//   }, [stockData, lineColor, dateRange])
 
-}
+//   if(!stockData) return null
 
-export default StockGraph;
+//   const dateFunc = (date) => {
+//     if (date === '1Y') {
+//       setTimeInterval('1Y')
+//       setDateRange(stockData)
+//     } else if (date === '6M') {
+//       setTimeInterval('6M')
+//       setDateRange(stockData.slice((stockData.length) / 2))
+//     } else if (date === '3M') {
+//       setTimeInterval('3M')
+//       setDateRange(stockData.slice(((stockData.length) / 4) * 3))
+//       setTimeInterval('1M')
+//       setDateRange(stockData.slice(((stockData.length) / 12) * 11))
+//     }
+//   }
+
+//   const handleClick = (price) => {
+//     setCurrentPrice(price.toFixed(2))
+//     // condition on state of dateRange
+//     console.log('stockdat price --------', stockData[0].price)
+//     setCurrentChange( Math.abs( (price - stockData[0].price).toFixed(2) ) )
+//     // calc percent change
+//     if (stockData[(stockData?.length) -1].price > stockData[0].price) {
+//       setCurrentPercentChg( Math.abs((((stockData[0].price - price) / price)*100).toFixed(5)))
+//     } else {
+//       setCurrentPercentChg( Math.abs((((price - stockData[0].price) / stockData[0].price)*100).toFixed(2)))
+//     }
+//     //set sign
+//     if (stockData[(stockData?.length) -1].price > stockData[0].price) {
+//       setSign('+')
+//     } else {
+//       setSign('-')
+//     }
+//     //set "Past"
+//     if (timeInterval == '1m') {
+//       setPast('Past Month')
+//     } else if (timeInterval == '3m') {
+//       setPast('Past 3 Months')
+//     } else if (timeInterval == '6m') {
+//       setPast('Past 6 Months')
+//     } else if (timeInterval == '1Y') {
+//       setPast('Past Year')
+//     }
+//   }
+
+
+//   return (
+//     <>
+//       <div id='stock-graph-container'>
+//         <div className='stock-ticker-container'>
+//           <div id="ticker">
+//                 <h1 id="">${currentPrice ? currentPrice : (stockData[(stockData?.length)-1]['price']).toFixed(2)}</h1>
+//           </div>
+//           <div id='ticker-change'>
+//             <p>{`${sign ? sign : stockData[(stockData?.length) -1].price > stockData[0].price ? '+' : '-'}$${ currentChange ? currentChange : Math.abs( (stockData[(stockData?.length) -1].price - stockData[0].price).toFixed(2) )}
+//               (${sign ? sign : stockData[(stockData?.length) -1].price > stockData[0].price ? '+' : '-'}${currentPercentChg ? currentPercentChg : Math.abs( (((stockData[0].price - stockData[(stockData?.length) -1].price)/ stockData[(stockData?.length) -1].price) * 100).toFixed(5) )  }%)`}
+//               { past ? past : 'Past Year'}
+//             </p>
+//           </div>
+//         </div>
+//         <LineChart width={730} height={250} data={dateRange ? dateRange : stockData}
+//           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+//           onMouseMove={(e) => handleClick(e.activePayload ? console.log(e?.activePayload[0].payload.price) : stockData[(stockData.length)-1].price)}
+
+//           >
+//           <XAxis dataKey="date" hide={true} />
+//           <YAxis dataKey="price" domain={['auto', 'auto']} hide={true} />
+//           <Tooltip />
+//           <Line type="monotone" dataKey="price" stroke={lineColor} strokeWidth={1.5} dot={false} />
+//         </LineChart>
+//         <div id='stock-graph-btns'>
+//           <div>
+//             <button type='button' value='1M' onClick={(e) => dateFunc(e.target.value)}>1M</button>
+//           </div>
+//           <div>
+//             <button type='button' value='3M' onClick={(e) => dateFunc(e.target.value)}>3M</button>
+//           </div>
+//           <div>
+//             <button type='button' value='6M' onClick={(e) => dateFunc(e.target.value)}>6M</button>
+//           </div>
+//           <div>
+//             <button type='button' value='1Y' onClick={(e) => dateFunc(e.target.value)}>1Y</button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+
+// }
+
+// export default StockGraph;
--- react-app/src/components/Stock/index.js
+++ react-app/src/components/Stock/index.js
@@ -7,6 +7,7 @@ import Buy from './Buy';
 import Sell from './Sell';
 import StockGraph from './StockGraph';
 import News from '../News';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 import './stock.css';
 
@@ -42,27 +43,120 @@ const Headers = ({ titles, currentTab, selectTab }) => {
 const Stock = () => {
     const { ticker_symbol } = useParams();
     const stock = useSelector(state => state.assets.stockData);
+    const stockOverview = stock.stock_info;
+    const stockData = stock.stock_data;
+    const user = useSelector(state => state.session.user);
     const stockTransactions = useSelector(state => state.transactions.transactionPrice)
     const closePrice = stockTransactions.price?.close.toFixed(2);
     const userShares = stockTransactions.shares?.[ticker_symbol];
 
-    const stockData = stock.stock_data;
-    const stockOverview = stock.stock_info;
+    const [dateRange, setDateRange] = useState(stockData);
+    const [lineColor, setLineColor] = useState("")
+    const [currentPrice, setCurrentPrice] = useState('')
+    const [currentChange, setCurrentChange] = useState('')
+    const [currentPercentChg, setCurrentPercentChg] = useState('')
+    const [sign, setSign] = useState('')
+    const [timeInterval, setTimeInterval] = useState('')
+    const [past, setPast] = useState('')
+    const [currentTab, setCurrentTab] = useState(0);
+    const[yearColor, setYearColor] = useState("#97ef0c")
+    const[oneMonthColor, setOneMonthColor] = useState('#353535')
+    const[threeMonthColor, setThreeMonthColor] = useState('#353535')
+    const[sixMonthColor, setSixMonthColor] = useState('#353535')
+
     const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user);
+
 
     const folders = [{ title: `Buy ${ticker_symbol}` }, { title: `Sell ${ticker_symbol}` }]
-    const [currentTab, setCurrentTab] = useState(0);
-    const folder = folders[currentTab];
     const titles = folders.map((folder) => folder.title);
 
-    useEffect(() => {
-        dispatch(stockInformation(ticker_symbol.toUpperCase()));
-    }, [dispatch, ticker_symbol]);
+    useEffect(async() => {
+       await dispatch(stockInformation(ticker_symbol.toUpperCase()));
+    }, []);
+
+    useEffect(async() => {
+        await dispatch(getPriceShares(ticker_symbol.toUpperCase()));
+    }, []);
+
+
+    useEffect( () => {
+        let stockDatePriceArr = dateRange ? dateRange : stockData;
+        if (stockDatePriceArr) {
+          if (stockDatePriceArr[0]["price"] > stockDatePriceArr[(stockDatePriceArr.length) - 1]["price"]) {
+            setLineColor("#dc436f"); //pink
+          } else {
+            setLineColor("#97ef0c"); //green
+          }
+        }
+    }, [stockData, lineColor, dateRange])
 
     useEffect(() => {
-        dispatch(getPriceShares(ticker_symbol.toUpperCase()));
-    }, [dispatch, ticker_symbol]);
+
+            if (timeInterval === '1Y') {
+
+                setDateRange(stockData)
+                setYearColor(lineColor)
+                setOneMonthColor("#353535")
+                setThreeMonthColor("#353535")
+                setSixMonthColor("#353535")
+              } else if (timeInterval === '6M') {
+
+                setDateRange(stockData.slice((stockData.length) / 2))
+                setYearColor("#353535")
+                setOneMonthColor("#353535")
+                setThreeMonthColor("#353535")
+                setSixMonthColor(lineColor)
+              } else if (timeInterval === '3M') {
+
+                setDateRange(stockData.slice(((stockData.length) / 4) * 3))
+                setYearColor("#353535")
+                setOneMonthColor("#353535")
+                setThreeMonthColor(lineColor)
+                setSixMonthColor("#353535")
+              } else if (timeInterval === '1M') {
+
+                setDateRange(stockData.slice(((stockData.length) / 12) * 11))
+                setYearColor("#353535")
+                setOneMonthColor(lineColor)
+                setThreeMonthColor("#353535")
+                setSixMonthColor("#353535")
+              }
+
+
+    }, [timeInterval, stockData, lineColor, yearColor, oneMonthColor, threeMonthColor, sixMonthColor])
+
+    if(!stockData) return null
+
+
+
+      const handleClick = (price) => {
+        setCurrentPrice(price?.toFixed(2))
+        // condition on state of dateRange
+        console.log('stockdat price --------', stockData[0].price)
+        setCurrentChange( Math.abs( (price - stockData[0].price).toFixed(2) ) )
+        // calc percent change
+        if (stockData[(stockData?.length) -1].price > stockData[0].price) {
+          setCurrentPercentChg( Math.abs((((stockData[0].price - price) / price)*100).toFixed(5)))
+        } else {
+          setCurrentPercentChg( Math.abs((((price - stockData[0].price) / stockData[0].price)*100).toFixed(2)))
+        }
+        //set sign
+        if (dateRange ? dateRange[0]['price'] < dateRange[dateRange.length -1]['price'] : stockData[(stockData?.length) -1].price > stockData[0].price) {
+          setSign('+')
+        } else {
+          setSign('-')
+        }
+        //set "Past"
+        if (timeInterval == '1M') {
+          setPast('Past Month')
+        } else if (timeInterval == '3M') {
+          setPast('Past 3 Months')
+        } else if (timeInterval == '6M') {
+          setPast('Past 6 Months')
+        } else if (timeInterval == '1Y') {
+          setPast('Past Year')
+        }
+      }
 
 
 
@@ -70,7 +164,46 @@ const Stock = () => {
         <div id='stock-container'>
             {stockOverview?.company_name}
             <div id='stock-graph'>
-                <StockGraph />
+            <div id='stock-graph-container'>
+                <div className='stock-ticker-container'>
+                <div id="ticker">
+                        <h1 id="">${currentPrice ? currentPrice : (stockData[(stockData?.length)-1]['price']).toFixed(2)}</h1>
+                </div>
+                <div id='ticker-change'>
+                    <p>{`${sign ? sign : stockData[(stockData?.length) -1].price > stockData[0].price ? '+' : '-'}$${ currentChange ? currentChange : Math.abs( (stockData[(stockData?.length) -1].price - stockData[0].price).toFixed(2) )}
+                    (${sign ? sign : stockData[(stockData?.length) -1].price > stockData[0].price ? '+' : '-'}${currentPercentChg ? currentPercentChg : Math.abs( (((stockData[0].price - stockData[(stockData?.length) -1].price)/ stockData[(stockData?.length) -1].price) * 100).toFixed(5) )  }%)`}
+                    { past ? past : 'Past Year'}
+                    </p>
+                </div>
+                </div>
+                <LineChart width={730} height={250} data={dateRange ? dateRange : stockData}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                onMouseMove={(e) => handleClick(e.activePayload ? e?.activePayload[0].payload.price : stockData[(stockData.length)-1].price)}
+                >
+                <XAxis dataKey="date" hide={true} />
+                <YAxis dataKey="price" domain={['auto', 'auto']} hide={true} />
+                <Tooltip />
+                <Line type="monotone" dataKey="price" stroke={lineColor} strokeWidth={1.5} dot={false} />
+                </LineChart>
+                <div id='stock-graph-btns'>
+                <div>
+                    <button style={{color: oneMonthColor}} type='button' value='1M' onClick={(e) => setTimeInterval('1M')}>1M</button>
+                </div>
+                <div>
+                    <button style={{color: threeMonthColor}} type='button' value='3M' onClick={(e) => setTimeInterval('3M')}>3M</button>
+                </div>
+                <div>
+                    <button style={{color: sixMonthColor}} type='button' value='6M' onClick={(e) => setTimeInterval('6M')}>6M</button>
+                </div>
+                <div>
+                    <button style={{color: yearColor}} type='button' value='1Y' onClick={(e) => setTimeInterval(e.target.value)}>1Y</button>
+                </div>
+                </div>
+            </div>
+
+
+
+
                 <section>
                     <div className='tabs'>
                         <Headers
@@ -130,3 +263,6 @@ const Stock = () => {
 }
 
 export default Stock;
+
+
+// dateRange ? dateRange[0]["price"] > dateRange[(dateRange.length) - 1]["price"] ? "#dc436f" : "#97ef0c" : stockData[0]["price"] > stockData[(stockData.length) - 1]["price"] ? "#dc436f" : "#97ef0c"
