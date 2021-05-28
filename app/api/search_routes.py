91c6f3a59bff28312e069af8384e68df3e05142e
Tran <tranle97@gmail.com>
2021-05-27 21:07:45 -0500

    Search comments

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -7,11 +7,16 @@ search_routes = Blueprint("search", __name__)
 def get_stocks():
     data = {}
     stocks = Asset.query.all()
+    
+    # this way to get stocks or second way uncommented
     # for stock in stocks:
     #     data[stock.ticker_symbol] = stock.company_name
-    # print(data,'==========data')
+    # print(data,'==========data') 
+    # data will = {'AAPL': 'Apple', 'MSFT': 'Microsoft', 'TSLA': 'Tesla',
+
     for i in range(0, len(stocks)):
         data[i] = {"ticker_symbol": stocks[i].ticker_symbol, "company_name": stocks[i].company_name}
    
     print('=========---------------------=data', data)
+    # data will = {0: {'ticker_symbol': 'AAPL', 'company_name': 'Apple'}, 1: {'ticker_symbol': 'MSFT', 'company_name': 'Microsoft'}, 2: {'ticker_symbol': 'TSLA', 'company_name': 'Tesla'}
     return data
