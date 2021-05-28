038d57c2a358e696a54571ab8a0292694c7abb16
Jessica T <jmthorn16@gmail.com>
2021-05-28 17:28:57 -0500

    sandbox to cloud

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -11,13 +11,12 @@ def get_stocks():
     # this way to get stocks or second way uncommented
     # for stock in stocks:
     #     data[stock.ticker_symbol] = stock.company_name
-    # print(data,'==========data') 
+    # print(data,'==========data')
     # data will = {'AAPL': 'Apple', 'MSFT': 'Microsoft', 'TSLA': 'Tesla',
 
     for i in range(0, len(stocks)):
-        data.append({"ticker_symbol": stocks[i].ticker_symbol, "company_name": stocks[i].company_name})
-   
-    # print('=========---------------------=data', data)
-    # data will = [0: {'ticker_symbol': 'AAPL', 'company_name': 'Apple'}, 1: {'ticker_symbol': 'MSFT', 'company_name': 'Microsoft'}, 2: {'ticker_symbol': 'TSLA', 'company_name': 'Tesla']
-    return {"stock_names": data} 
+        data.append(
+            {"ticker_symbol": stocks[i].ticker_symbol, "company_name": stocks[i].company_name})
 
+    # data will = [0: {'ticker_symbol': 'AAPL', 'company_name': 'Apple'}, 1: {'ticker_symbol': 'MSFT', 'company_name': 'Microsoft'}, 2: {'ticker_symbol': 'TSLA', 'company_name': 'Tesla']
+    return {"stock_names": data}
