0833e962f36ac82e61ed1863a62f3cbd8a280a0c
Tran <tranle97@gmail.com>
2021-05-28 02:39:01 -0500

    Fix search results on click

--- react-app/src/components/NavBar/Search/index.js
+++ react-app/src/components/NavBar/Search/index.js
@@ -8,52 +8,55 @@ const Search = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const stocks = useSelector(state => state.search.searchInfo);
-  // console.log('==========searchstocks', stocks)
+
   const tickerSymbols = stocks.stock_names?.map(stock => (stock['ticker_symbol']))
   const companyNames = stocks.stock_names?.map(stock => (stock['company_name']))
 
-  console.log('--------------------', tickerSymbols)
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
   const handleChange = e => {
-    // e.preventDefault();
     setSearchTerm(e.target.value);
   };
-
+  
   useEffect(() => {
-    dispatch(stockSearch());
-  }, [dispatch])
-
-  useEffect(() => {
-    const results = tickerSymbols?.filter(symbol => symbol.toLowerCase().match(searchTerm));
+    const results = tickerSymbols?.filter(symbol => symbol.toLowerCase().includes(searchTerm));
     if (!results) setSearchTerm("");
     setSearchResults(results);
   }, [searchTerm]);
 
+  // useEffect(() => {
+  //   if (!focus && searchTerm) {
+  //     setSearchTerm(" ");
+  //     setSearchResults([]);
+  //   }
+  // }, [searchTerm])
+  
   return (
     <div id='search-component'>
       <input
         type="text"
         placeholder='Search'
         onFocus={useEffect(() => {
+          setSearchTerm(" ");
           dispatch(stockSearch())
         }, [dispatch])}
         onChange={handleChange}
+        onClick={() => {
+          setSearchTerm("");
+          setSearchResults([]);
+        }}
         value={searchTerm} />
-      <ul>
-        {searchResults && searchResults.map(item => (
-          <div className='stock-search-items' onClick={e => {
-            e.preventDefault();
-            setSearchResults([]);
-            setSearchTerm("");
-            history.push(`/stocks/${item}`);
-          }}>
-            {searchResults && <NavLink to={`/stocks/${item}`}>{item}</NavLink>}
-          </div>
-        ))}
-        {!searchResults && <div></div>}
-      </ul>
+        <ul>
+          {searchResults?.map(item => (
+            <div className='stock-search-items' onClick={() => {
+              setSearchTerm(" ");
+              history.push(`/stocks/${item}`);
+            }}>
+              <NavLink to={`/stocks/${item}`}>{item}</NavLink>
+            </div>
+          ))}
+        </ul>
     </div>
   )
 }
