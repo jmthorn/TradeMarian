9669311180bf3fdc2c6864b0183495b7ff506fdb
Jessica T <jmthorn16@gmail.com>
2021-06-30 15:50:49 -0500

    add cascade delete

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -3,68 +3,68 @@ from app.models import db, Transaction
 
 def seed_transactions():
     t1 = Transaction(
-        asset_id = 1,
-        user_id = 1,
-        share_quantity = 5,
-        price_per_share = 125.30,
-        buy_sell = True
+        asset_id=1,
+        user_id=1,
+        share_quantity=5,
+        price_per_share=125.30,
+        buy_sell=True
     )
 
     t2 = Transaction(
-        asset_id = 2,
-        user_id = 1,
-        share_quantity = 7,
-        price_per_share = 244.98,
-        buy_sell = True
+        asset_id=2,
+        user_id=1,
+        share_quantity=7,
+        price_per_share=244.98,
+        buy_sell=True
     )
     t3 = Transaction(
-        asset_id = 3,
-        user_id = 1,
-        share_quantity = 10,
-        price_per_share = 578.99,
-        buy_sell = True
+        asset_id=3,
+        user_id=1,
+        share_quantity=10,
+        price_per_share=578.99,
+        buy_sell=True
     )
     t4 = Transaction(
-        asset_id = 4,
-        user_id = 1,
-        share_quantity = 4,
-        price_per_share = 229.14,
-        buy_sell = True
+        asset_id=4,
+        user_id=1,
+        share_quantity=4,
+        price_per_share=229.14,
+        buy_sell=True
     )
     t5 = Transaction(
-        asset_id = 5,
-        user_id = 1,
-        share_quantity = 2,
-        price_per_share = 21,
-        buy_sell = True
+        asset_id=5,
+        user_id=1,
+        share_quantity=2,
+        price_per_share=21,
+        buy_sell=True
     )
     t6 = Transaction(
-        asset_id = 6,
-        user_id = 1,
-        share_quantity = 3,
-        price_per_share = 172.40,
-        buy_sell = True
+        asset_id=6,
+        user_id=1,
+        share_quantity=3,
+        price_per_share=172.40,
+        buy_sell=True
     )
     t7 = Transaction(
-        asset_id = 7,
-        user_id = 1,
-        share_quantity = 11,
-        price_per_share = 110.99,
-        buy_sell = True
+        asset_id=7,
+        user_id=1,
+        share_quantity=11,
+        price_per_share=110.99,
+        buy_sell=True
     )
     t8 = Transaction(
-        asset_id = 7,
-        user_id = 1,
-        share_quantity = -11,
-        price_per_share = 110.99,
-        buy_sell = False
+        asset_id=7,
+        user_id=1,
+        share_quantity=-11,
+        price_per_share=110.99,
+        buy_sell=False
     )
     t9 = Transaction(
-        asset_id = 5,
-        user_id = 1,
-        share_quantity = 4,
-        price_per_share = 21,
-        buy_sell = True
+        asset_id=5,
+        user_id=1,
+        share_quantity=4,
+        price_per_share=21,
+        buy_sell=True
     )
 
     db.session.add(t1)
@@ -80,5 +80,5 @@ def seed_transactions():
 
 
 def undo_transactions():
-    db.session.execute('TRUNCATE transactions RESTART IDENTITY')
+    db.session.execute('TRUNCATE transactions CASCADE RESTART IDENTITY')
     db.session.commit()
